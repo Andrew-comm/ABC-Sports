5b4090c5fb97fb83644b0eed47e234e248d7ff9c
andrea <ndrwrono2001@gmail.com>
2024-06-02 15:01:22 +0300

    simple sports site

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    navToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('nav-active');
+    });
+});
